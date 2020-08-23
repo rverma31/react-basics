7bfef4d97a16d90e639964711fc5d96803b5a467
Ritika Verma <ritika7659@gmail.com>
2020-08-23 21:01:40 +0530

    added footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './Header/Header';
+import Footer from './Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <h2>React Application</h2>
       <Header></Header>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <footer><h2>Footer Component</h2></footer>
+            </div>
+        );
+    }
+}
+
+export default Footer;
